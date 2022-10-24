7b36c6c838fc9dcd23004a55acccf1f47f188fe0
Zverina <danii.zvorugin@gmail.com>
2022-10-24 13:10:47 +0700

    asdf

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import React,{useEffect} from 'react';
 import { useTelegram } from './components/hooks/useTelegram';
 import Header from './components/Header/Header';
+import {Route, Routes} from 'react-router-dom';
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
 
 
 function App() {
@@ -14,7 +17,10 @@ function App() {
     return (
         <div className="App">
             <Header/>
-            <button onClick={onToggleButton}>Toggle</button>
+            <Routes>
+                <Route index element={<ProductList/>}/>
+                <Route path={'form'} element={<Form/>}/>
+            </Routes>
         </div>
     );
 }
---
+++ client/src/components/Form/Form.jsx
@@ -0,0 +1,39 @@
+import React,{useState, useEffect} from 'react';
+import './Form.css';
+import { useTelegram } from './../hooks/useTelegram';
+
+const Form = () => {
+    const [country, setCountry] = useState('')
+    const [street, setStreet] = useState('')
+    const [subject, setSubject] = useState('')
+    const {tg} = useTelegram()
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Отправить данные',
+        })
+    }, [])
+
+    useEffect(() => {
+        if(!country || !street){
+            tg.MainButton.hide()
+        }else{
+            tg.MainButton.show()
+        }
+    }, [country, street])
+    
+    return (
+        <div className='form'>
+            <h3>Введите ваши данные:</h3>
+            <input className={'input'} type="text" placeholder='Страна' onChange={(e) => setCountry(e.target.value)} value={country}/>
+            <input className={'input'} type="text" placeholder='Улица' onChange={(e) => setStreet(e.target.value)} value={street}/>
+
+            <select value={subject} onChange={(e) => setSubject(e.target.value)} className='select'>
+                <option value={"physical"}>Физ. лицо</option>
+                <option value={"legal"}>Юр. лицо</option>
+            </select>
+        </div>
+    );
+}
+
+export default Form;
